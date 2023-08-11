d1ff9a8ba388121b5685ce80bcb13b163ead5540
X3NNY <xenny@vip.qq.com>
2023-08-12 02:07:28 +0800

    5.3 作业 部分

--- 代码/Tree.cpp
+++ 代码/Tree.cpp
@@ -54,14 +54,14 @@ void InOrder2(BiTree T) { // 中序遍历 左根右
             p = p->lchild;                  // 继续遍历左子树
         } else {
             Pop(S, x);                      // 出栈，即上一个左子树已经遍历完，此时遍历上一轮压栈的子树的右子树
-            visit(p);                       // 访问根结点
+            visit((BiTree)x);                       // 访问根结点
             p = ((BiTree)x)->rchild;
         }
     }
     puts("");
 }
 
-void PostOrder2(BiTree T) { // 中序遍历 左右根
+void PostOrder2(BiTree T) { // 后序遍历 左右根
     SqStack S;
     InitStack(S);
     BiTree p = T, r;
@@ -129,6 +129,73 @@ int LCA(SqTree T, int i, int j) {     // 5. 找到最近公共祖先
     return -1;
 }
 
+// 5.3 作业
+
+void CustomOrder(BiTree T) { // 4. 自下而上，从右到左 就是RLN
+    if (T != NULL) {
+        CustomOrder(T->rchild);
+        CustomOrder(T->lchild);
+        visit(T);
+    }
+}
+
+int GetHeight(BiTree T) { // 5. 用非递归方法求高度
+    /**
+     * 用层次遍历的方法，每层高度加一，或者用栈保存结点以及深度
+    */
+    if (!T) return 0;
+    BiTree p;
+    ElemType x;
+    BiTree Q[MaxSize];
+    int height = 0,
+        front = -1,                 // 队首
+        rear = -1,                  // 队尾
+        last = 0;                   // 记录上一层最后结点位置
+    Q[++rear] = T;
+    while(front < rear) {
+        p = Q[++front];             // 出队
+        if (p->lchild != NULL) {
+            Q[++rear] = p->lchild;  // 入队
+        }
+        if (p->rchild != NULL) {    
+            Q[++rear] = p->rchild;  // 入队
+        }
+        if(front == last) {         // 如果遍历到上一轮最后一个结点
+            height++;               // 层数+1
+            last = rear;            // 同时代表当前层的所有结点都已入队，更新最后一个结点位置
+        }
+    }
+    return height;
+}
+
+BiTree PreInOrderCreate(ElemType A[], ElemType B[], int ls, int le, int rs, int re) { // 6. 用前序和中序序列建树
+    /**
+     * 就是将自己转化变成代码
+     * 先序第一个位置是根，然后在中序中找根的位置，将树划分为两半
+     * 然后就可以递归的对左右子树分别建树了
+     * ls，le分别代表A中的起始结束位置，rs，re同理
+    */ElemType pre[] = {1,2,4,5,3};ElemType in[] = {4,2,5,1,3};
+    int i;
+    BiTree T = (BiTNode*)malloc(sizeof(BiTNode));
+    T->data = A[ls];                                       // 前序的第一个就是根结点
+    for (i = rs; B[i] != T->data; i++);                    // 寻找根结点在中序中的位置
+
+    int llen = i-rs, rlen = re-i;                          // 左右子树长度
+    if (llen) {                                            // 如果左子树序列长度不为0，也就是存在左子树
+        T->lchild = PreInOrderCreate(A, B, ls+1, ls+llen, rs, rs+llen-1);
+                                                           // 此时，先序中的左子树序列便是根结点后llen长度
+                                                           // 中序便是开始位置到根结点前一个共llen长度
+    } else T->lchild = NULL;
+
+    if (rlen) {
+        T->rchild = PreInOrderCreate(A, B, le-rlen+1, le, re-rlen+1, re);
+                                                           // 同理操作右子树，此时先序是根左右，所以右子树序列应该出现在最后面，
+                                                           // 则是从后往前rlen个位置，同理中序也是从后往前rlen个位置
+    } else T->rchild = NULL;
+    return T;
+}
+
+
 int main() {
     BiTree T = (BiTree)malloc(sizeof(BiTNode));
     T->data = 1;
@@ -151,10 +218,17 @@ int main() {
     T->lchild->rchild->lchild = NULL;
     T->lchild->rchild->rchild = NULL;
 
+    // PreOrder(T);
+    // InOrder2(T);
     // PostOrder2(T);
     // PostOrder(T);
 
-    LevelOrder(T);
+    // LevelOrder(T);
 
+    // int x = GetHeight(T);printf("%d\n", x);
+
+    ElemType pre[] = {1,2,4,5,3};ElemType in[] = {4,2,5,1,3};
+    BiTree T2 = PreInOrderCreate(pre, in, 0, 4, 0, 4);
+    PreOrder(T2);InOrder(T2);
     return 0;
 }
