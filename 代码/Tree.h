6b3e0e5e911d534b127aa1115aec460bf7ff3b74
X3NNY <xenny@vip.qq.com>
2023-08-12 02:19:22 +0800

    5.4 树和森林

--- 代码/Tree.h
+++ 代码/Tree.h
@@ -1,11 +1,11 @@
 #include "stdafx.h"
 #define MAXLEN 256
 
-typedef struct {
+typedef struct {                        // 顺序存储的二叉树，双亲和孩子看作满二叉树计算
     ElemType data[MAXLEN];
 }SqTree;
 
-typedef struct BiTNode {                // 链式存储
+typedef struct BiTNode {                // 链式存储的二叉树
     ElemType data;                      // 数据域
     struct BiTNode *lchild, *rchild;    // 左右孩子指针
 }BiTNode, *BiTree;
@@ -15,6 +15,21 @@ typedef struct ThreadNode {             // 线索二叉树
     struct ThreadNode *lchild, *rchild; // 左、右孩子指针
     int ltag, rtag;                     // 左、右线索标识
 }ThreadNode, *ThreadTree;
+
+typedef struct {                        // 双亲表示法 结点
+    ElemType data;
+    int parent;                         // 存储双亲下标
+}PTNode;
+typedef struct {                        // 双亲表示法 树
+    PTNode nodes[MAXLEN];
+}PTree;
+
+typedef struct CSNode {                 // 孩子兄弟表示法
+    ElemType data;
+    struct CSNode *firstchid,           // 存储第一个孩子指针
+                  *nextsibling;         // 存储自己的下一个兄弟指针
+}CSNode, *CSTree;
+
 
 
 void visit(BiTree T) {
