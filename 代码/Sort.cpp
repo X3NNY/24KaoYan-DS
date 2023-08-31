1bcc2cffe2b1fe15c4325474fdd7c8df6fcf9a58
X3NNY <xenny@vip.qq.com>
2023-08-31 18:57:20 +0800

    8.3作业

--- 代码/Sort.cpp
+++ 代码/Sort.cpp
@@ -82,6 +82,149 @@ void QuickSort(ElemType A[], int low, int high) {   // 快速排序
     }
 }
 
+
+// 8.3 作业
+
+void swap(ElemType &a, ElemType &b) {
+    ElemType tp = a;
+    a = b;
+    b = tp;
+}
+
+void Bubble2Sort(ElemType A[], int n) {     // 2. 双向冒泡排序
+    int l = 0, r = n-1;
+    bool flag = true;                       // 记录某轮是否发生交换
+    while (l < r && flag) {                 // 双向逼近
+        flag = false;
+        for (int i = l; i < r; i++) {       // 从前往后
+            if (A[i] > A[i+1]) {            // 如果前面大于后面则交换
+                swap(A[i], A[i+1]);
+                flag = true;
+            }
+        }
+        r--;                                // 此时一定把最大值冒到r位置了，可以r--
+        for (int i = r; i > l; i--) {       // 从后往前
+            if (A[i] < A[i-1]) {            // 如果后面小于前面则交换
+                swap(A[i], A[i-1]);
+                flag = true;
+            }
+        }
+        l++;                                // 此时把最小的冒到l位置了，可以l++
+    }
+}
+
+void QuickMove(ElemType A[], int n) {       // 3. 将所有奇数移动到偶数前
+    /**
+     * 同理可以利用快排思路
+     * 这里相当于一趟划分
+    */
+    int l = 0, r = l-1;
+    while (l < r) {                         // 对区间进行划分
+        while (l < r && A[l] % 2 != 0) l++; // 从左往右找偶数
+        while (l < r && A[r] % 2 != 1) r--; // 从右往左找奇数
+        if (l < r) {                        // 如果偶数在奇数左边则交换
+            swap(A[l], A[r]);
+        }
+        l++, r--;                           // 缩减区间 继续下一轮交换
+    }
+}
+
+void QuickSort2(ElemType A[], int low, int high) {  // 4. 每次随机选取枢轴的快排
+    if (low < high) {                               // 不为空
+        int index = low + rand()%(high-low+1);      // 随机选取枢轴
+        swap(A[index], A[low]);                     // 还是放到low位置，方便处理
+        ElemType pivot = A[low];                    // 这样枢轴还是第一个元素
+        int l = low, r = high;
+        while (l < r) {                             // 不断从两端逼近
+            while (l < r && A[r] >= pivot) r--;     // 从右往左找小于pivot元素的位置
+            A[l] = A[r];                            // 移动到左边
+            while (l < r && A[l] <= pivot) l++;     // 从左往右找大于pivot元素的位置
+            A[r] = A[l];                            // 移动到右边
+        }
+        A[l] = pivot;                               // 最后 l=r 也就是pivot的位置
+
+        QuickSort(A, low, l-1);                     // 对左右部分分别递归
+        QuickSort(A, l+1, high);             
+    }
+}
+
+ElemType KthElement(ElemType A[], int low, int high, int k) {   // 5. 找到第k小元素
+    /**
+     * 要求O(n)找到第k小，不能使用排序再返回的方式
+     * 但是我们可以利用快排划分的方式，让k-1个比m小的在左边，则k位就是我们要的
+    */
+    int pivot = A[low];                                         // 第一个作为枢轴
+    int ltp = low, htp = high;
+    while (low < high) {
+        while (low < high && A[high] >= pivot) high--;          // 从右往左找比pivot小的
+        A[low] = A[high];                                       // 换到最左边
+        while (low < high && A[low] <= pivot) low++;            // 从左往右找比pivot大的
+        A[high] = A[low];                                       // 换到最右边
+    }
+    A[low] = pivot;                                             // 最终l=r就是pivot该放的位置
+    if (low == k) {                                             // 如果放在第k位，则pivot就是我们需要的
+        return A[low];
+    } else if (low > k) {                                       // 如果第k位在左边
+        return KthElement(A, ltp, low-1, k);                    // 则去左边部分继续寻找
+    } else {
+        return KthElement(A, low+1, htp, k);                    // 否则则去右边部分寻找
+    }
+}
+
+ElemType FlagArrange(ElemType A[], int n) {  // 6. 荷兰国旗问题：将颜色序列按红白蓝顺序排列 O(n)
+    /**
+     * 利用三指针操作，这里设0，1，2分别为红白蓝。
+     * 中间指针为遍历指针，其他指针维护边界，通过不断将中间指针和边界进行交换使得红蓝分别移动到左右两端
+     * 这样中间剩下的就是白了。
+    */
+    int i = 0, j = 0, k = n-1;
+    while (j <= k) {
+        switch (A[j]) {
+        case 0:                         // 如果j是红，则应该放到左边，则交换到i指针位置，同时边界++
+            swap(A[i], A[j]);i++,j++;
+            break;
+        case 1:                         // 如果是白，则放在中间无问题，j++
+            j++;break;
+        case 2:
+            swap(A[j], A[k]); k--;      // 如果是蓝，则应放在后面，边界--
+        }
+    }
+}
+
+ElemType SetSlice(ElemType A[], int n) {    // 7. 划分成块，要求两块长度只差尽量小，两块元素和之差尽量大
+    /**
+     * 显然我们把小的放一边，大的放另一边即可
+     * 这样的话变成了把n/2位放到既定位置，然后小的放左边，大的放右边，也就变成了上面那题的做法
+    */
+    int low = 0, high = n-1, flag = true;
+    while (flag) {
+        int ltp = low, htp = high;
+        int pivot = A[low];                                 // 第一个作为枢轴
+        while (low < high) {
+            while (low < high && A[high] >= pivot) high--;  // 从右往左找比pivot小的
+            A[low] = A[high];                               // 换到最左边
+            while (low < high && A[low] <= pivot) low++;    // 从左往右找比pivot大的
+            A[high] = A[low];                               // 换到最右边
+        }
+        A[low] = pivot;                                     // 最终l=r就是pivot该放的位置
+        if (low == n/2) {                                   // 如果放在第n/2位，则划分完成
+            flag = false;
+        } else if (low > n/2) {                             // 如果n/2位在左边，则去左边找
+            low = ltp;
+            high = low-1;
+        } else {                                            // 否则去右边找
+            low = low+1;
+            high = htp;
+        }
+    }
+
+    ElemType s1 = 0, s2 = 0;
+
+    for (int i = 0; i < n/2; i++) s1 += A[i];
+    for (int i = n/2; i < n; i++) s2 += A[i];
+    return s2-s1;
+}
+
 int main() {
     ElemType A[] = {1152,28269,95422,28902,59695,51038,92653,22568,62664,65812,85193,6970,117181,108792,99284,97047,52711,18498,57024,62044};
     int n = 20;
